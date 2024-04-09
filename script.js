12778e74515c6e52ddc53bbac67b59ef90c5dffe
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-09 16:33:57 -0600

    Added code to script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+window.onload = function () {
+  setGame()
+}
+
+function setGame() {
+  // set up tye grid for the game board in html
+  for(let i = 0; i < 9; i++) {
+
+    
+    let tile = document.createElement("div")
+    tile.id = i.toString()
+  }
+}
