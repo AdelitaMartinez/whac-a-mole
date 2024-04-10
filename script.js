75193029ce7cf0410b12052953f263489cde06bd
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-10 10:20:02 -0600

    Added script to file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let currMoleTile;
 
 window.onload = function () {
   setGame()
@@ -13,4 +13,17 @@ function setGame() {
     tile.id = i.toString()
     document.getElementById("board").appendChild(tile)
   }
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9)
+    return num.toString()
+}
+
+function setMole() {
+
+  let mole = document.createElement("img")
+  mole.src = "./monty-mole.png"
+
+  let num = getRandomTile()
 }
